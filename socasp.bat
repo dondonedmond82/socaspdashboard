11c0d0c0425a6c7d698960b3c98bd6a07e923bd6
Dondon Edmond <dondonkonamabaye@gmail.com>
2025-09-20 06:47:59 +0100

    Initial commit

--- socasp.bat
+++
@@ -1,3 +0,0 @@
-cd C:\projet\socasp\socasp_02_08_2025
-
-panel serve socasp.py --template=login.html --basic-auth .\credentials.json --cookie-secret toor --allow-websocket-origin=lucretiayenam-pc --port 80 --autoreload
